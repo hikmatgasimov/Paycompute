0dc156625706e6a29ca349692aa37b0e3ace7b00
gasimovhikmat <gasimov.hikmat95@gmail.com>
2021-10-22 11:26:17 +0400

    first commit

---
+++ Paycompute.Service/IEmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Paycompute.Entity;
+
+namespace Paycompute.Service
+{
+    public interface IEmployeeService
+    {
+        Task CreateAsync(Employee employee);
+        Task UpdateAsync(Employee employee);
+        Task UpdateAsync(int employeeId);
+        Task DeleteAsync(int employeeId);
+        Employee GetById(int employeeId);
+        decimal UnionFees(int Id);
+        decimal StudentLoanRepaymentAmount(int Id, decimal totalAmount);
+        IEnumerable<Employee> GetALL();
+        IEnumerable<SelectListItem> GetALLEmployeesForPayroll();
+
+    }
+}
---
+++ Paycompute.Service/Implementation/EmployeeService.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Paycompute.Entity;
+using Paycompute.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paycompute.Service
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly ApplicationDbContext _context;
+        private decimal studentLoanAmount;
+        public EmployeeService(ApplicationDbContext _context)
+        {
+            this._context = _context;
+        }
+
+        public async Task CreateAsync(Employee employee)
+        {
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int employeeId)
+        {
+            var employee = GetById(employeeId);
+            _context.Remove(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public IEnumerable<Employee> GetALL() => _context.Employees.AsNoTracking().OrderBy(emp=>emp.FullName);
+
+        public IEnumerable<SelectListItem> GetALLEmployeesForPayroll()
+        {
+            return GetALL().Select(emp => new SelectListItem()
+            {
+                Text = emp.FullName,
+                Value = emp.Id.ToString()
+            });
+        }
+
+        public Employee GetById(int employeeId) => _context.Employees.Where(e => e.Id == employeeId).FirstOrDefault();
+
+
+        public decimal StudentLoanRepaymentAmount(int Id, decimal totalAmount)
+        {
+            var employee = GetById(Id);
+            if (employee.StudentLoan == StudentLoan.Yes && totalAmount > 1750 && totalAmount < 2000)
+            {
+                studentLoanAmount = 15m;
+            }
+            else if (employee.StudentLoan == StudentLoan.Yes && totalAmount >= 2000 && totalAmount < 2250)
+            {
+                studentLoanAmount = 38m;
+            }
+            else if (employee.StudentLoan == StudentLoan.Yes && totalAmount >= 2250 && totalAmount < 2500)
+            {
+                studentLoanAmount = 60m;
+            }
+            else if (employee.StudentLoan == StudentLoan.Yes && totalAmount >= 2500)
+            {
+                studentLoanAmount = 83m;
+            }
+            else
+            {
+                studentLoanAmount = 0m;
+            }
+            return studentLoanAmount;
+        }
+
+        public decimal UnionFees(int Id)
+        {
+            var employee = GetById(Id);
+            var fee = employee.UnionMember == UnionMember.Yes ? 10m : 0m;
+            return fee;
+        }
+
+        public async Task UpdateAsync(Employee employee)
+        {
+            _context.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int employeeId)
+        {
+            var employee = GetById(employeeId);
+            _context.Update(employee);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
